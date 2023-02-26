951a3a8ca3234f51a7d405a56d82e62d88ff2523
Tomasz Woytkowiak <tomas.woytkowiak1505@gmail.com>
2023-02-26 23:30:46 +0100

    created calculate.js and 1st constructor variables what are loaded from index.html

---
+++ calculate.js
@@ -0,0 +1,6 @@
+class Calculate{
+    constructor() {
+        this.level = document.getElementById("current-level").value;
+        this.infamyPool = document.getElementById("infamy-pool-leveling").checked;
+    }
+}
